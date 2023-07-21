182490418e13449566bd445fa2085bd6df3651a1
eunbum kim <icesnake72@gmail.com>
2023-07-22 02:03:07 +0900

    String.format(Locale.KOREA, 수정

--- app/src/main/java/com/example/hellotoast/MainActivity.java
+++ app/src/main/java/com/example/hellotoast/MainActivity.java
@@ -7,22 +7,19 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.util.Timer;
+import java.util.Locale;
+
 
 public class MainActivity extends AppCompatActivity {
     private int mCount = 0;
     private TextView mShowCount;
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mShowCount = (TextView) findViewById(R.id.show_count);
-
-
+        mShowCount = findViewById(R.id.show_count);
     }
 
     public void showToast(View view) {
@@ -35,6 +32,6 @@ public class MainActivity extends AppCompatActivity {
         ++mCount;
 
         if (mShowCount != null)
-            mShowCount.setText(Integer.toString(mCount));
+            mShowCount.setText(String.format(Locale.KOREA,"%d", mCount)); //   Integer.toString(mCount));
     }
 }
